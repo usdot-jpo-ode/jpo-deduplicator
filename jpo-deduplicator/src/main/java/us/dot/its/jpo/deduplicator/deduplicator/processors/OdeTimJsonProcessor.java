319b117f2781ecc2dfac995df1ac4733f1d6ad09
Drew Johnston <drewjj901@gmail.com>
2025-08-05 02:38:37 -0600

    Removed redundant logic for TIM dedup processing

--- jpo-deduplicator/src/main/java/us/dot/its/jpo/deduplicator/deduplicator/processors/OdeTimJsonProcessor.java
+++ jpo-deduplicator/src/main/java/us/dot/its/jpo/deduplicator/deduplicator/processors/OdeTimJsonProcessor.java
@@ -7,7 +7,6 @@ import java.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import us.dot.its.jpo.asn.j2735.r2024.TravelerInformation.TravelerInformationMessageFrame;
 import us.dot.its.jpo.deduplicator.DeduplicatorProperties;
 import us.dot.its.jpo.ode.model.OdeMessageFrameData;
 import us.dot.its.jpo.ode.model.OdeMessageFrameMetadata;
@@ -63,21 +62,6 @@ public class OdeTimJsonProcessor extends DeduplicationProcessor<OdeMessageFrameD
                     newMessage == null || newMessage.getPayload() == null
                     || newMessage.getPayload().getData() == null) {
                 logger.warn("One or both TIM messages have null payload or data, treating as non-duplicate");
-                return false;
-            }
-
-            // Check if the TIM packet IDs are the same
-            TravelerInformationMessageFrame oldTim = (TravelerInformationMessageFrame) lastMessage.getPayload().getData();
-            TravelerInformationMessageFrame newTim = (TravelerInformationMessageFrame) newMessage.getPayload().getData();
-            String oldTimPacketId = oldTim.getValue().getPacketID().getValue();
-            String newTimPacketId = newTim.getValue().getPacketID().getValue();
-
-            if (!oldTimPacketId.equals(newTimPacketId)) {
-                return false;
-            }
-
-            // Check if the message count of the new TIM is identical to the old TIM
-            if (newTim.getValue().getMsgCnt().getValue() != oldTim.getValue().getMsgCnt().getValue()) {
                 return false;
             }
         } catch(Exception e){
